a18f50baf45eff99414d10481df27c2497147983
Hudson Menezes <hudson.bm23@gmail.com>
2023-08-15 17:22:24 -0300

    feat: fonts installed and added

--- App.tsx
+++ App.tsx
@@ -6,12 +6,3 @@ import Index from "./src/Index";
 export default function App() {
   return <Index />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/Index.tsx
+++ src/Index.tsx
@@ -1,9 +1,40 @@
 import React from "react";
 import { Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
+
+import {
+  useFonts,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+} from "@expo-google-fonts/poppins";
+
+import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Text>Wallet App</Text>
     </View>
   );
